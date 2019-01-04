6eef36e9aee576f91ae360ea897b89d78f832114
Jörn Krenzer <joern.krenzer@gmx.de>
2019-01-04 15:10:34 +0100

    Alternative lazy-calculation of dates. Will this work out?

--- src/strategy/Task.py
+++ src/strategy/Task.py
@@ -8,10 +8,36 @@ from DAG import Node
 
 class TimeSpan:
         def __init__(self, earliest: datetime = None, latest: datetime = None):
-            self.earliest = earliest if earliest is not None else datetime.now()
-            self.latest = latest if latest is not None else datetime.now()
-            
-        #TODO Link times if empty
+            self._earliest = earliest
+            self._latest = latest
+
+        def _getEarliest(self):
+            return min(self.asList())
+
+        def _getLatest(self):
+            return max(self.asList())
+
+        def asList(self):
+            return [ value if value is not None else datetime.now() for value in  self.asRawList() ]
+
+        def asRawList(self):
+            return [self._earliest, self._latest]
+
+        def _setEarliest(self, value):
+            self._earliest = value
+
+        def _setLatest(self, value):
+            self._latest = value
+
+        def reset(self):
+            self._earliest = None
+            self._latest = None
+
+        def isEmpty(self):
+            return self._earliest is None and self._latest is None
+
+        earliest = property(_getEarliest, _setEarliest)
+        latest = property(_getLatest, _setLatest)
 
 class Task(Node):
 
@@ -19,45 +45,66 @@ class Task(Node):
         super(Task, self).__init__(parents=parents, children=children)
         self.name = name
         self.duration = duration
+        self.pinned = False
         if isinstance(start, TimeSpan):
-            self.start = start
+            self.manualStart = start
         else:
-            self.start = TimeSpan()
+            self.manualStart = TimeSpan()
         if isinstance(end, TimeSpan):
-            self.end = end
+            self.manualEnd = end
         else:
-            self.end = TimeSpan()
-            
-    def calculate(self):
-        for root in self.getRootNodes():
-            root.calculateForwards()
-        for leaf in self.getLeafNodes():
-            leaf.calculateBackwards()
+            self.manualEnd = TimeSpan()
 
-    def calculateForwards(self):
-        self.calculateEarliestEnd()
-        for child in self._children:
-            child.calculateForwards()
-            
-    def calculateBackwards(self):
+    def _getStart(self):
+        if self.isARoot():
+            return TimeSpan(self.calculateEarliestStart(), None)
+        else:
+            return TimeSpan(self.calculateEarliestStart(), self.calculateLatestStart())
+
+    def _getEnd(self):
         if self.isALeaf():
-            self.end.latest = self.end.earliest
-        self.calculateLatestStart()
-        for parent in self._parents:
-            parent.calculateBackwards()
+            return TimeSpan(self.calculateEarliestEnd(), None)
+        else:
+            return TimeSpan(self.calculateEarliestEnd(), self.calculateLatestEnd())
+
+    start = property(_getStart,None)
+    end = property(_getEnd,None)
+
+    def noDatesSet(self):
+        return all([node.manualStart.isEmpty() for node in self.getAll()]) and all([node.manualEnd.isEmpty() for node in self.getAll()])
+
+    def raiseIfNoDatesSet(self):
+        if self.noDatesSet():
+            raise SyntaxError("You must set at least one date as fixpoint for solving!")
 
     def calculateEarliestStart(self):
+        self.raiseIfNoDatesSet()
+        if self.manualStart._earliest is not None:
+            return self.manualStart._earliest
         if self.hasParents():
-            self.start.earliest = max([ parent.end.earliest for parent in self._parents])
-        
+            return max([parent.calculateEarliestEnd() for parent in self._parents])
+        else:
+            return self.calculateLatestStart()
+
     def calculateLatestEnd(self):
+        self.raiseIfNoDatesSet()
+        if self.manualEnd._latest is not None:
+            return self.manualEnd._latest
         if self.hasChildren():
-            self.end.latest = min([ child.start.latest for child in self._children])
+            return min([child.calculateLatestStart() for child in self._children])
+        else:
+            return self.calculateEarliestEnd()
 
     def calculateEarliestEnd(self):
-        self.calculateEarliestStart()
-        self.end.earliest = self.start.earliest + self.duration
+        self.raiseIfNoDatesSet()
+        if self.manualEnd._earliest is not None:
+            return self.manualEnd._earliest
+        else:
+            return self.calculateEarliestStart() + self.duration
 
     def calculateLatestStart(self):
-        self.calculateLatestEnd()
-        self.start.latest = self.end.latest - self.duration
+        self.raiseIfNoDatesSet()
+        if self.manualStart._latest is not None:
+            return self.manualStart._latest
+        else:
+            return self.calculateLatestEnd() - self.duration
