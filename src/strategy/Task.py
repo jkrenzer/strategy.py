1cfb5ca9eaf7f91665ebf0b489534c71bb82e76f
Jörn Krenzer <joern.krenzer@gmx.de>
2019-01-04 01:24:02 +0100

    Got calculation running, somehow

--- src/strategy/Task.py
+++ src/strategy/Task.py
@@ -7,13 +7,15 @@ from DAG import Node
 #         self.latest
 
 class TimeSpan:
-        def __init__(self, earliest: datetime = datetime.now(), latest: datetime = datetime.now()):
-            self.earliest = earliest
-            self.latest = latest
+        def __init__(self, earliest: datetime = None, latest: datetime = None):
+            self.earliest = earliest if earliest is not None else datetime.now()
+            self.latest = latest if latest is not None else datetime.now()
+            
+        #TODO Link times if empty
 
 class Task(Node):
 
-    def __init__(self, name, duration: timedelta = timedelta(), *, start=None, end=None, parents: List['Task'] = [], children: List['Task'] = []):
+    def __init__(self, name, duration: timedelta, *, start=None, end=None, parents: List['Task'] = None, children: List['Task'] = None):
         super(Task, self).__init__(parents=parents, children=children)
         self.name = name
         self.duration = duration
@@ -25,21 +27,37 @@ class Task(Node):
             self.end = end
         else:
             self.end = TimeSpan()
+            
+    def calculate(self):
+        for root in self.getRootNodes():
+            root.calculateForwards()
+        for leaf in self.getLeafNodes():
+            leaf.calculateBackwards()
 
-    def calculateForward(self):
+    def calculateForwards(self):
         self.calculateEarliestEnd()
         for child in self._children:
-            child.calculateForward()
-
-    def setEarliestStartFromParent(self):
-        startEarliest = self.start.earliest
+            child.calculateForwards()
+            
+    def calculateBackwards(self):
+        if self.isALeaf():
+            self.end.latest = self.end.earliest
+        self.calculateLatestStart()
         for parent in self._parents:
-            if parent.start.earliest < startEarliest:
-                startEarliest = parent.start.earliest
-        self.start.earliest = startEarliest
+            parent.calculateBackwards()
+
+    def calculateEarliestStart(self):
+        if self.hasParents():
+            self.start.earliest = max([ parent.end.earliest for parent in self._parents])
+        
+    def calculateLatestEnd(self):
+        if self.hasChildren():
+            self.end.latest = min([ child.start.latest for child in self._children])
 
     def calculateEarliestEnd(self):
+        self.calculateEarliestStart()
         self.end.earliest = self.start.earliest + self.duration
 
     def calculateLatestStart(self):
+        self.calculateLatestEnd()
         self.start.latest = self.end.latest - self.duration
