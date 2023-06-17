fff96369ced3f2791e6436f437d15c3a5eab9e83
Jörn Krenzer <joern.krenzer@gmx.de>
2023-06-17 10:54:10 +0000

    Add coverage reporting

--- test/__main__.py
+++ test/__main__.py
@@ -1,5 +1,5 @@
 from strategy import logging
-
+from pathlib import Path
 
 from configargparse import ArgParser, YAMLConfigFileParser
 from unittest import TestLoader
@@ -21,6 +21,8 @@ rootParser.add('--log-level', default='debug', help="set loglevel",  # type: ign
                choices=logging.log_levels)
 rootParser.add('-v','--verbose', help='be verbose', action='count', default=0)  # type: ignore
 rootParser.add('-t','--tap', help='output results in TAP protocol', action='store_true')  # type: ignore
+rootParser.add('-C', '--coverage-report', help='generate a coverage report in the given format', choices=['html', 'json', 'lcov', 'xml'], type=str.lower)  # type: ignore
+rootParser.add('--coverage-output', help='generate the output in this folder or file. HTML needs a directory here', default='./.coverage_report', type=Path)
 
 if __name__ == '__main__':
     #    logging.basicConfig(level=logging.DEBUG)
@@ -32,7 +34,41 @@ if __name__ == '__main__':
 
     if options.tap:
         from pycotap import TAPTestRunner
-        TAPTestRunner().run(tests)
+        testRunner = TAPTestRunner()
     else:
         from unittest import TextTestRunner
-        TextTestRunner(verbosity=options.verbose).run(tests)
+        testRunner = TextTestRunner(verbosity=options.verbose)
+
+    if options.coverage_report:
+        from coverage import Coverage
+        cov = Coverage(
+            branch=True
+        )
+        cov.start()
+        testRunner.run(tests)
+        cov.stop()
+        if options.coverage_report == 'html':
+            cov.html_report(
+                directory= str(options.coverage_output.stem)
+            )
+        elif options.coverage_report == 'json':
+            if options.coverage_output.suffixes == []:
+                options.coverage_output /= 'report.json'
+            cov.json_report(
+                outfile = str(options.coverage_output),
+                pretty_print=True
+            )
+        elif options.coverage_report == 'lcov':
+            if options.coverage_output.suffixes == []:
+                options.coverage_output /= 'report.lcov'
+            cov.lcov_report(
+                outfile = str(options.coverage_output),
+            )
+        elif options.coverage_report == 'xml':
+            if options.coverage_output.suffixes == []:
+                options.coverage_output /= 'report.xml'
+            cov.xml_report(
+                outfile = str(options.coverage_output),
+            )
+    else:
+        testRunner.run(tests)
